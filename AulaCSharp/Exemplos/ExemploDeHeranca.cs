8f341186f1d499ba992fb7ec0e0e7a815e51d4a8
Erick <erickeek@hotmail.com>
2020-08-14 15:58:44 -0300

    exemplos de herança e classe abstrata

---
+++ AulaCSharp/Exemplos/ExemploDeHeranca.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace AulaCSharp.Exemplos
+{
+    public abstract class Pessoa
+    {
+        public string Nome { get; set; }
+    }
+    public class PessoaFisica : Pessoa
+    {
+        public string Cpf { get; set; }
+    }
+    public class PessoaJuridica : Pessoa
+    {
+        public string Cnpj { get; set; }
+    }
+
+    public class Funcionario
+    {
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public string Saudacao()
+        {
+            return $"Oi, sou funcionário #{Numero}";
+        }
+    }
+
+    public class ComissaoFuncionario : Funcionario
+    {
+        public double TaxaDeComissao { get; set; }
+    }
+
+    public class InstrumentoMusical
+    {
+        public virtual void Tocar()
+        {
+            Console.WriteLine("Tocando um instrumento");
+        }
+    }
+
+    public class Guitarra : InstrumentoMusical
+    {
+        public override void Tocar()
+        {
+            Console.WriteLine("Aprendendo o nome das cordas");
+        }
+    }
+
+    public class Bateria : InstrumentoMusical
+    {
+        public override void Tocar()
+        {
+            Console.WriteLine("Aprendendo coordenção motora");
+        }
+    }
+
+    public abstract class ExportarRelatorioDeAmigos
+    {
+        protected string NomeDoArquivo { get; private set; }
+
+        protected ExportarRelatorioDeAmigos(string nomeDoArquivo)
+        {
+            NomeDoArquivo = nomeDoArquivo;
+        }
+
+        public abstract void Exportar();
+    }
+
+    public class ExportarRelatorioDeAmigosEmPdf : ExportarRelatorioDeAmigos
+    {
+        public ExportarRelatorioDeAmigosEmPdf(string nomeDoArquivo) : base(nomeDoArquivo)
+        {
+        }
+
+        public override void Exportar()
+        {
+            Console.WriteLine("Vou criar um pdf");
+        }
+    }
+
+    public class ExportarRelatorioDeAmigosEmExcel : ExportarRelatorioDeAmigos
+    {
+        public ExportarRelatorioDeAmigosEmExcel(string nomeDoArquivo) : base(nomeDoArquivo)
+        {
+        }
+
+        public override void Exportar()
+        {
+            Console.WriteLine("Vou criar um excel");
+        }
+    }
+}
--- AulaCSharp/Program.cs
+++ AulaCSharp/Program.cs
@@ -1,4 +1,4 @@
-﻿using AulaCSharp.Exercicios;
+﻿using AulaCSharp.Exemplos;
 using System;
 
 namespace AulaCSharp
@@ -8,7 +8,31 @@ namespace AulaCSharp
         // função principal, chamada automaticamente
         public static void Main()
         {
-            ExercicioAgenda.Executar();
+            InstrumentoMusical instrumentoMusical = new InstrumentoMusical();
+            InstrumentoMusical guitarra = new Guitarra();
+            InstrumentoMusical bateria = new Bateria();
+
+            var instrumentos = new[] { instrumentoMusical, guitarra, bateria };
+            foreach (var instrumento in instrumentos)
+            {
+                instrumento.Tocar();
+            }
+
+            bool exportarEmPdf = false;
+            string nomeDoArquivo;
+            ExportarRelatorioDeAmigos exportador;
+            if (exportarEmPdf)
+            {
+                nomeDoArquivo = "arquivo.pdf";
+                exportador = new ExportarRelatorioDeAmigosEmPdf(nomeDoArquivo);
+            }
+            else
+            {
+                nomeDoArquivo = "arquivo.xls";
+                exportador = new ExportarRelatorioDeAmigosEmExcel(nomeDoArquivo);
+            }
+
+            exportador.Exportar();
 
             Console.ReadKey();
         }
